ed2eac1685c4124bd150b1c00368988cb51a4ced
Sehara1022 <seharabanu.mulla@coderview.io>
2024-10-24 14:20:09 +0530

    deleting files

--- backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "guardduty-statefile-bucket"
-    key     = "guardduty/terraform.tfstate"
-    region  = "eu-west-2"
-    encrypt = true
-  }
-}
--- modules/guardduty/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "guardduty_enabler" {
-  description = "guardduty enabler resource ID"
-  value       = aws_guardduty_detector.guardduty.id
-}
